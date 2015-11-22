f51a10223eba531219663f7f551f96b7f6626e8b
sntshakaza <ntshakazas@yahoo.com>
2015-11-22 16:59:37 +0200

    added files for project 2

---
+++ Plot3.R
@@ -0,0 +1,32 @@
+## Initializing NEI and SCC
+if(!exists("NEI")){
+  NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
+
+## if ggplot2 is not installed, install it by removing the ## on the line below
+##install.packages("ggplot2")
+
+library(ggplot2)
+
+# Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
+# which of these four sources have seen decreases in emissions from 1999 2008 for Baltimore City? 
+# Which have seen increases in emissions from 1999 2008? 
+# Use the ggplot2 plotting system to make a plot answer this question.
+
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+
+aggregatedTotalByYearAndType <- aggregate(Emissions ~ year + type, subsetNEI, sum)
+
+
+
+png("plot3.png", width=640, height=480)
+g <- ggplot(aggregatedTotalByYearAndType, aes(year, Emissions, color = type))
+g <- g + geom_line() +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions in Baltimore City, Maryland (fips == "24510") from 1999 to 2008')
+print(g)
+dev.off()
